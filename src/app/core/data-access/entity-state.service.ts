3af14baca446d19ff8c77d70196981dd1fdeb417
Joachim Urbain <joachimurbain.git@proton.me>
2025-03-29 14:16:31 +0100

    fixing stale data in state machine

--- src/app/core/data-access/entity-state.service.ts
+++ src/app/core/data-access/entity-state.service.ts
@@ -2,7 +2,7 @@ import { computed, signal } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Subject, BehaviorSubject, switchMap, map, EMPTY, tap } from 'rxjs';
+import { Subject, BehaviorSubject, switchMap, map, EMPTY, tap, finalize, exhaustMap } from 'rxjs';
 import { CrudApiService } from './crud-api.service';
 import { BaseEntity } from './base-entity.model';
 
@@ -61,47 +61,17 @@ export abstract class EntityStateService<T extends BaseEntity> {
 				error: (err) => this.error$.next(err),
 			});
 
-		this.getOne$
-			.pipe(
-				switchMap((id) => {
-					if (!id) {
-						return EMPTY;
-					}
-					const cached = this.state().fullItems[id];
-					if (cached) {
-						return EMPTY;
-					}
-					return this._apiService.getById(id);
-				})
-			)
-			.subscribe({
-				next: (item) =>
-					this.state.update((state) => ({
-						...state,
-						fullItems: {
-							...state.fullItems,
-							[item.id]: item,
-						},
-						error: null,
-					})),
-				error: (err) => this.error$.next(err),
-			});
-
 		// this.getOne$
 		// 	.pipe(
 		// 		switchMap((id) => {
-		// 			if (this.state().fullItems[id] || this.loadingIds.has(id)) {
+		// 			if (!id) {
 		// 				return EMPTY;
 		// 			}
-
-		// 			this.loadingIds.add(id);
-
-		// 			return this._apiService.getById(id).pipe(
-		// 				tap({
-		// 					next: () => this.loadingIds.delete(id),
-		// 					error: () => this.loadingIds.delete(id),
-		// 				})
-		// 			);
+		// 			const cached = this.state().fullItems[id];
+		// 			if (cached) {
+		// 				return EMPTY;
+		// 			}
+		// 			return this._apiService.getById(id);
 		// 		})
 		// 	)
 		// 	.subscribe({
@@ -117,16 +87,40 @@ export abstract class EntityStateService<T extends BaseEntity> {
 		// 		error: (err) => this.error$.next(err),
 		// 	});
 
+		this.getOne$
+			.pipe(
+				switchMap((id) => {
+					if (!id || this.state().fullItems[id] || this.loadingIds.has(id)) {
+						return EMPTY;
+					}
+
+					this.loadingIds.add(id);
+
+					return this._apiService.getById(id).pipe(finalize(() => this.loadingIds.delete(id)));
+				})
+			)
+			.subscribe({
+				next: (item) =>
+					this.state.update((state) => ({
+						...state,
+						fullItems: {
+							...state.fullItems,
+							[item.id]: item,
+						},
+						error: null,
+					})),
+				error: (err) => this.error$.next(err),
+			});
+
 		this.create$
 			.pipe(
 				takeUntilDestroyed(),
-				switchMap((item) => this._apiService.create(item))
+				exhaustMap((item) => this._apiService.create(item))
 			)
 			.subscribe({
 				next: (newItem) => {
 					this.state.update((state) => ({
 						...state,
-						// summaries: [...state.summaries, newItem],
 						fullItems: {
 							...state.fullItems,
 							[newItem.id]: newItem,
@@ -142,15 +136,21 @@ export abstract class EntityStateService<T extends BaseEntity> {
 		this.update$
 			.pipe(
 				takeUntilDestroyed(),
-				switchMap((item) => this._apiService.update(item))
+				exhaustMap((item) => this._apiService.update(item))
 			)
 			.subscribe({
-				next: (updatedItem) =>
+				next: (updatedItem) => {
 					this.state.update((state) => ({
 						...state,
+						fullItems: {
+							...state.fullItems,
+							[updatedItem.id]: updatedItem,
+						},
 						summaries: state.summaries.map((p) => (p.id === updatedItem.id ? updatedItem : p)),
 						error: null,
-					})),
+					}));
+					this.getAll$.next();
+				},
 				error: (err) => this.error$.next(err),
 			});
 
@@ -164,9 +164,20 @@ export abstract class EntityStateService<T extends BaseEntity> {
 					this.state.update((state) => ({
 						...state,
 						summaries: state.summaries.filter((i) => i.id !== removedId),
+						fullItems: this.removeItem(state.fullItems, removedId),
 						error: null,
 					})),
 				error: (err) => this.error$.next(err),
 			});
+	}
+
+	removeItem<T>(record: Record<number, T>, removeId: number): Record<number, T> {
+		const result: Record<number, T> = {};
+		for (const id in record) {
+			if (Number(id) !== removeId) {
+				result[Number(id)] = record[id];
+			}
+		}
+		return result;
 	}
 }
